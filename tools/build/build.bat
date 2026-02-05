8da231122cb2027640e51fc7eabaca3b4de4d9c5
Meghan-Rossi <56671765+Meghan-Rossi@users.noreply.github.com>
2026-02-05 23:16:44 +0000

    automatically sort ntf_modular includes

--- tools/build/build.bat
+++ tools/build/build.bat
@@ -1,2 +1,7 @@
 @echo off
+cd %~dp0/../../ntf_modular
+sort include.dm /o include2.dm
+del include.dm
+move include2.dm include.dm >nul
+cd %~dp0
 "%~dp0\..\bootstrap\node.bat" --experimental-modules "%~dp0\build.js" %*
